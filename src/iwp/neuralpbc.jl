13cd0ce037958fd077a860ee9b75fb1dfd546732
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2022-01-12 10:53:46 -0700

    more weights

--- src/iwp/neuralpbc.jl
+++ src/iwp/neuralpbc.jl
@@ -13,7 +13,8 @@ end
 function policyfrom(P::NeuralPBC; umax=Inf, lqrmax=umax)
     u_neuralpbc(x,p) = begin
         sq1, cq1, sq2, cq2, q1dot, q2dot = x
-        if (1-cq1 < 1-cosd(10)) && abs(q1dot) < 1
+        if (1-cq1 < 1-cosd(15)) && abs(q1dot) < pi/3
+        # if (1-cq1 < 1-cosd(10)) && abs(q1dot) < pi/4
             effort = -dot(LQR, [sq1, sq2, q1dot, q2dot])
             return clamp(effort, -lqrmax, lqrmax)
         else
@@ -35,23 +36,25 @@ end
 
 function train!(::ReactionWheelPendulum, pbc::NeuralPBC, ps; 
     tf=3.0, dt=0.1, umax=0.5, lqrmax=1.5,
-    batchsize=4, maxiters=1000, replaybuffer=batchsize*5
+    batchsize=4, maxiters=1000, replaybuffer=5
 )
     sys = ParametricControlSystem(ReactionWheelPendulum(), pbc, umax=umax, lqrmax=lqrmax)
+    # sys = ParametricControlSystem{false}(
+    #     eomwrap, (x,θ)->pbc(x,θ), 6
+    # )
     dist(x) = begin
         sq1, cq1, sq2, cq2, q1dot, q2dot = x
-        return (1-cq1) + q1dot^2 + q2dot^2
+        return 2(1-cq1) + (q1dot^2)/4 + (q2dot^2)/8
     end
-    loss = SetDistanceLoss(dist, wrap(zeros(4)), 0.1)
+    loss = SetDistanceLoss(dist, wrap(zeros(4)), 1/100)
     losstypestr = typeof(loss).name.name |> string
-    tspan = (zero(tf), tf)
+    tspan = (tf/2, tf) # (zero(tf), tf)
     prob = ODEProblem(sys, ps, tspan)
     optimizer = Flux.ADAM()
-    # sampler = MLBasedESC.CustomDagger(prob, zeros(4), wrap)
-    sampler(batchsize, ps) = customdagger(prob, batchsize, ps, tf=6.0)
+    sampler(batchsize, ps) = customdagger(prob, batchsize, ps, tf=tf)
     data = reduce(vcat, sampler(batchsize, ps) for _=1:replaybuffer)
     for nepoch in 1:maxiters
-        data = vcat(last(data,replaybuffer-batchsize), sampler(batchsize, ps))
+        data = vcat(last(data,(replaybuffer-1)*batchsize), sampler(batchsize, ps))
         dataloader = Flux.Data.DataLoader(data; batchsize=batchsize, shuffle=true)
         max_batch = round(Int, dataloader.imax / dataloader.batchsize, RoundUp)
         epochloss = 0
@@ -59,18 +62,29 @@ function train!(::ReactionWheelPendulum, pbc::NeuralPBC, ps;
         for batch in dataloader
             gs, ls = gradient(loss, prob, batch, ps; dt=dt)
             epochloss = +(epochloss, ls)
-            if !isnothing(gs)
+            if !isnothing(gs) && !any(isnan, gs)
+                # gs[end-6+1:end] .= 0
                 Flux.Optimise.update!(optimizer, ps, gs)
             end
             MLBasedESC.bstatus(nbatch, max_batch, ls)
             nbatch += 1
+            if iszero(nbatch % 10)
+                plot(pbc, ps, umax=umax, lqrmax=lqrmax, tf=tf, 
+                    x0=unwrap(first(batch)));
+            end
         end
-        plot(pbc, ps, umax=umax, lqrmax=lqrmax, tf=20.0, 
-            x0=[3,0*randn(),0randn(),0randn()]);
+        # plot(pbc, ps, umax=umax, lqrmax=lqrmax, tf=batchsize*tf, 
+        #             x0=[3,0pi*randn(),0randn(),0randn()]);
         MLBasedESC.estatus(losstypestr, nepoch, epochloss/nbatch, maxiters)
     end
 end
 
+function customdagger(::ReactionWheelPendulum, pbc::NeuralPBC, n, θ, tf; kwargs...)
+    sys = ParametricControlSystem(ReactionWheelPendulum(), pbc; kwargs...)
+    tspan = (zero(tf), tf)
+    prob = ODEProblem(sys, θ, tspan)
+    return customdagger(prob, n, θ; tf=tf)
+end
 function customdagger(prob, n, θ; tf=last(prob.tspan))
     xstar = zeros(4)
     Σ = LinearAlgebra.I
@@ -78,13 +92,13 @@ function customdagger(prob, n, θ; tf=last(prob.tspan))
     if rand(Bool)
         return map(wrap, eachcol(rand(D,n)))
     else
-        xinit = rand(D)
+        xinit = rand(D)/2
         xinit[1:2] *= pi
         x0 = wrap(xinit)
         tspan = (zero(tf), tf)
-        tsave = range(first(tspan), last(tspan), length=101)
+        tsave = range(first(tspan), last(tspan), length=n)
         x = trajectory(remake(prob, tspan=tspan), x0, θ, saveat=tsave)
-        return collect.(rand(collect(eachcol(x)), n))
+        return collect(Vector{eltype(x)},eachcol(x))
     end
 end
 
@@ -119,10 +133,6 @@ function plot(evolution::Tuple{AbstractMatrix,AbstractVector}; out=true)
     return fig
 end
 function plot(pbc::NeuralPBC, θ; out=true, kwargs...)
-    #=
-    Usage: 
-    plot(evaluate(pbc, ps, umax=2.0, tf=20.0, x0=[pi*randn(), pi*randn(),0,0]))
-    =#
     evolution = evaluate(pbc, θ; kwargs...)
     fig = plot(evolution, out=false)
     X = range(-pi, pi, length=50)
@@ -134,9 +144,9 @@ function plot(pbc::NeuralPBC, θ; out=true, kwargs...)
             Z[ix,iy] = pbc.Hd(x,θ)[1]
         end
     end
-    # Z = [pbc.Hd(wrap([x,y,0,0]),θ)[1] for x in X, y in Y]
-    Axis(fig[3,2], title="Hd")
-    contour!(X,Y,Z)
+    ax, ct = contour(fig[3,2][1,1], X,Y,Z, colormap=:gnuplot)
+    ax.title = "Hd"
+    Colorbar(fig[3,2][1,2], ct)
     out && save("out.png", fig)
     return fig
 end
